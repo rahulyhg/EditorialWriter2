48bf52a9ea31d63ab7867c7ca9630969bda80f3d
bunny1997 <priyank20patel@gmail.com>
2017-03-30 06:43:13 -0700

    fixed 1st item in editorial list

--- app/src/main/java/app/vacabulary/editorialwriter/gamefever/editorialwriter/EditorialListActivity.java
+++ app/src/main/java/app/vacabulary/editorialwriter/gamefever/editorialwriter/EditorialListActivity.java
@@ -135,7 +135,9 @@ public class EditorialListActivity extends AppCompatActivity {
 
 
         int insertPosition = editorialListArrayList.size();
-        editorialGeneralInfoArraylist.remove(editorialGeneralInfoArraylist.size()-1);
+        if(insertPosition != 0) {
+            editorialGeneralInfoArraylist.remove(editorialGeneralInfoArraylist.size() - 1);
+        }
 
         for (EditorialGeneralInfo editorialGeneralInfo : editorialGeneralInfoArraylist){
             editorialListArrayList.add(insertPosition ,editorialGeneralInfo);
